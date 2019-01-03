bc90c0096fb37ca0a39cc2daaf47241edde16f5d
dustytavern <46342671+dustytavern@users.noreply.github.com>
2019-01-03 01:23:29 -0800

    added templates to templates folder

---
+++ template/top.php
@@ -0,0 +1,19 @@
+<?php if(CONF_BGVID == "ON"){ ?>
+<section class="face">
+<!-- Specify the path to your video file, because this is video from Facepunch website :) -->
+<div class="fullvideo">
+        <video loop="" muted="" autoplay="" playsinline="">
+                <source src="bg.mp4" type="video/mp4">
+        </video>
+</div>
+<?php ;}else{ ?>
+<section class="face" style="background-image: url( 'img/bg_0.jpg' );">
+<?php ;};?>
+                <div class="face-body">
+                        <div class="container text-center">
+                                <h1 class="face-title one"><?php echo SRV_NAME; ?></h1>
+                                <h2 class="face-subtitle"><?php echo SRV_SLOGAN; ?></h2>
+                        </div>
+                </div>
+</section>
+
